14637a610d75ea72cc26cf85ea8b56e00f28c389
bhs24542 <24542@burnside.school.nz>
2026-05-29 01:14:30 +1200

    Import database

--- 21st Century Heavy-lift Launch Vehicle/app.py
+++ 21st Century Heavy-lift Launch Vehicle/app.py
@@ -1,24 +1,6 @@
 #docstring - 21st Century Heavy-lift Launch Vehicle Database Application
-#import database
+#Import database
 import sqlite3
 DATABASE = 'rockets.db'
 
 
-#functions
-def print_all_rockets():
-    #print all the rockets with their organizations and nationality
-    db = sqlite3.connect(DATABASE)
-    cursor = db.cursor()
-    sql = """
-    SELECT Rockets.rocket_name, Organizations.org_name, Organizations.nationality 
-    FROM Rockets 
-    JOIN Organizations ON Rockets.org_id = Organizations.org_id;
-    """
-    cursor.execute(sql)
-    results = cursor.fetchall()
-    #loop through all the results
-    print(f"rocket_name              organization        nationality")
-    for rocket in results:
-        print(f"{rocket[0]:<25}{rocket[1]:<20}{rocket[2]:<15}")
-    #loop finish here
-    db.close()
