1b0fb90ef2010e20b459a61089296fc0add5d3c5
bhs24542 <24542@burnside.school.nz>
2026-05-30 02:17:05 +1200

    Final code

--- 21st Century Heavy-lift Launch Vehicle/app.py
+++ 21st Century Heavy-lift Launch Vehicle/app.py
@@ -1,4 +1,4 @@
-#docstring - 21st Century Heavy-lift Launch Vehicle Database Application
+#docstring - Jervis Xu - 21st Century Heavy-lift Launch Vehicle Database Application
 #Import database
 import sqlite3
 DATABASE = "rockets.db"
